8e3fb1e007da67623fe1f6a258da6b8dd911a074
evan miller <evanchancemiller@gmail.com>
2019-01-30 15:48:06 -0500

    feat: setup routing, homepage, and global styles

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,13 +7,21 @@ import { AboutComponent } from "./about/about.component";
 import { ContactComponent } from "./contact/contact.component";
 import { ProjectsComponent } from "./projects/projects.component";
 import { AppRoutingModule } from "./app-routing.module";
+import { HomeComponent } from "./home/home.component";
+import { NavComponent } from "./global/nav/nav.component";
+import { SocialComponent } from "./global/social/social.component";
+import { SkillsComponent } from './skills/skills.component';
 
 @NgModule({
   declarations: [
     AppComponent,
+    NavComponent,
     AboutComponent,
     ContactComponent,
-    ProjectsComponent
+    ProjectsComponent,
+    HomeComponent,
+    SocialComponent,
+    SkillsComponent
   ],
   imports: [BrowserModule, AppRoutingModule, FormsModule],
   providers: [],
