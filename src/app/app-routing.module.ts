cc639b8fd2e1751032750bcbd20b2c7b99f44d84
evan miller <evanchancemiller@gmail.com>
2019-02-04 15:06:40 -0500

    feat: app component styles and routes added

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -11,7 +11,7 @@ const routes: Routes = [
   { path: "", component: HomeComponent },
   { path: "about", component: AboutComponent },
   { path: "projects", component: ProjectsComponent },
-  { path: "contact", component: ContactComponent }
+  { path: "contact", component: ContactComponent },
   { path: "skills", component: SkillsComponent }
 ];
 
