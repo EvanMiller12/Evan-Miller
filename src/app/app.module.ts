eb62002bdb3f112cd4a8190436adc3754cab41b5
evan miller <evanchancemiller@gmail.com>
2019-02-21 11:54:36 -0500

    feat: update copy and refactored views

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,20 +3,18 @@ import { NgModule } from "@angular/core";
 import { FormsModule } from "@angular/forms"; // <-- NgModel lives here
 
 import { AppComponent } from "./app.component";
-import { AboutComponent } from "./about/about.component";
 import { ContactComponent } from "./contact/contact.component";
 import { ProjectsComponent } from "./projects/projects.component";
 import { AppRoutingModule } from "./app-routing.module";
 import { HomeComponent } from "./home/home.component";
 import { NavComponent } from "./global/nav/nav.component";
 import { SocialComponent } from "./global/social/social.component";
-import { SkillsComponent } from './skills/skills.component';
+import { SkillsComponent } from "./skills/skills.component";
 
 @NgModule({
   declarations: [
     AppComponent,
     NavComponent,
-    AboutComponent,
     ContactComponent,
     ProjectsComponent,
     HomeComponent,
